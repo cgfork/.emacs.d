4bf144a04ca4d352a7582ef397f957e29d9972cb
C_G <cg.fork@gmail.com>
2019-10-31 10:19:17 +0800

    fix clojure mode

--- conf/+paredit.el
+++ conf/+paredit.el
@@ -11,7 +11,7 @@
 (with-eval-after-load 'paredit
   (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
   (add-hook 'lisp-mode-hook 'enable-paredit-mode)
-  (add-hook 'clojure-mode 'enable-paredit-mode)
+  (add-hook 'clojure-mode-hook 'enable-paredit-mode)
   (add-hook 'eval-expression-minibuffer-setup-hook 'enable-paredit-mode))
 
 (provide '+paredit)
