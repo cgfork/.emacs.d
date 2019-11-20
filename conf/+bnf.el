6c14b4de2b5eb79a7c13543cf52112323688f7d9
c_g <cg.fork@gmail.com>
2019-11-20 14:38:56 +0800

    add bnf highlight

---
+++ conf/+bnf.el
@@ -0,0 +1,10 @@
+;;; package --- ABNF and BNF -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(when (cgfork/try-install 'bnf-mode)
+  )
+
+
+(provide '+bnf)
+;;; +bnf.el ends here
--- conf/+org.el
+++ conf/+org.el
@@ -32,6 +32,22 @@
    (plantuml . t)
    (shell . t)))
 
+(define-generic-mode 'bnf-mode
+ () ;; comment char: inapplicable because # must be at start of line
+ nil ;; keywords
+ '(
+   ("^#.*" . 'font-lock-comment-face) ;; comments at start of line
+   ("^<[^ \t\n]*?>" . 'font-lock-function-name-face) ;; LHS nonterminals
+   ("<[^ \t\n]*?>" . 'font-lock-builtin-face) ;; other nonterminals
+   ("::=" . 'font-lock-const-face) ;; "goes-to" symbol
+   ("\|" . 'font-lock-warning-face) ;; "OR" symbol
+   )
+ '("\\.bnf\\'") ;; filename suffixes
+ nil ;; extra function hooks
+ "Major mode for BNF highlighting.")
+
+(add-to-list 'org-src-lang-modes '("bnf" . bnf))
+
 (unless (featurep 'ob) (require 'ob))
 (unless (featurep 'ox-html) (require 'ox-html))
 (unless (featurep 'ox-publish) (require 'ox-publish))
