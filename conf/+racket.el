878256af7722d06d0cf0a1e22bddff13b5876413
c_g <cg.fork@gmail.com>
2020-01-17 14:28:51 +0800

    update racket mode

--- conf/+racket.el
+++ conf/+racket.el
@@ -4,6 +4,7 @@
 
 (cgfork/try-install 'racket-mode)
 (cgfork/after-load 'racket-mode
+  (cgfork/open-paredit 'racket-mode-hook)
   (add-hook 'racket-mode-hook
 	    (lambda ()
 	      (define-key racket-mode-map (kbd "<f5") 'racket-run))))
