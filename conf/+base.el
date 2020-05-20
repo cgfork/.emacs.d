e8c4b61144acc98d158f060731e66f7b325d4a7d
cgfork <cg.fork@gmail.com>
2020-05-20 08:30:38 +0800

    sync

--- conf/+base.el
+++ conf/+base.el
@@ -31,7 +31,7 @@
    column-number-mode t
    auto-save-default nil
    make-backup-files nil
-   cursor-type 'bar
+   cursor-type 'box
    ns-pop-up-frames nil
    tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))
 
